e4f34602124ce51b1ee4c1dbc5441df5894e234c
mRameez110 <temprary87@gmail.com>
2025-04-24 12:41:26 +0500

    comment out the app.listen for live

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,9 @@ app.use((req, res, next) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+module.exports = app;
